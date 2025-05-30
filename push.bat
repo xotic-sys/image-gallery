f386108e65fde9e83a5702ba77611e868abcf4ce
John <O49w2G7R8z6l3J@hotmail.com>
2025-05-29 19:56:54 -0700

    Auto-commit on 2025-05-29 at 19:%e:%f

---
+++ push.bat
@@ -0,0 +1,19 @@
+﻿@echo off
+REM Change directory to your repo folder
+cd /d A:/database/Documents/py/data
+
+REM Stage all changes
+git add .
+
+REM Create a commit message with current date and time
+for /f "tokens=1-4 delims=/-: " %%a in ("%date% %time%") do (
+    set commitMsg=Auto-commit on %%a-%%b-%%c at %%d:%%e:%%f
+)
+
+REM Commit changes
+git commit -m "%commitMsg%"
+
+REM Push changes to main branch
+git push origin main
+
+pause
